1dc775275d647d2fc7d99c60c546cde7a2bf007b
Jakov Peric <jakov.peric.ext@siemens-convergence.com>
2017-04-05 16:55:52 +0200

    colour and size changes

--- myScript.js
+++ myScript.js
@@ -1,8 +1,3 @@
-var myCanvas = document.getElementById("canvas");
-var myContext = myCanvas.getContext("2d");
-
-var RECT_WIDTH = 40
-var RECT_HEIGHT = RECT_WIDTH
 
 
 class Rectangle {
@@ -14,14 +9,13 @@ class Rectangle {
     }
     draw() {
         if (this.checked) {
-            this.context.fillStyle = "#000000"
+            this.context.fillStyle = ALIVE_COLOUR
             this.context.fillRect(this.x, this.y, RECT_HEIGHT, RECT_WIDTH)
         } else {
             //clear previous filled rect
             this.context.fillStyle = "#FFFFFF"
             this.context.fillRect(this.x, this.y, RECT_HEIGHT, RECT_WIDTH)
-            this.context.rect(this.x, this.y, RECT_HEIGHT, RECT_WIDTH)
-        }
+         }
     }
     onclick() {
         if (this.checked) {
@@ -37,30 +31,15 @@ class Rectangle {
 
 
 
-var grid = Array(20);
-for (var i = 0; i < grid.length; i++) {
-    grid[i] = Array(20);
-}
 
-
-for (var i = 0; i < grid.length; i++) {
-    for (var j = 0; j < grid[i].length; j++) {
-        grid[i][j] = new Rectangle(myContext, i * RECT_WIDTH, j * RECT_HEIGHT)
-        grid[i][j].draw()
+function drawGrid() {
+    myContext.strokeStyle = BORDER_COLOUR
+    for (var i = 0; i < grid.length; i++) {
+        for (var j = 0; j < grid.length; j++) {
+            myContext.rect(i * RECT_HEIGHT, j * RECT_WIDTH, RECT_HEIGHT, RECT_WIDTH);
+         }
     }
 }
-myCanvas.addEventListener('mousedown', function (evt) {
-    var mousePos = getMousePos(canvas, evt);
-    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-    console.log(message)
-    var message = 'rectangle: ' + Math.floor(mousePos.x / 50) + ',' + Math.floor(mousePos.y / 50)
-    console.log(message)
-    var rect = grid[Math.floor(mousePos.x / RECT_WIDTH)][Math.floor(mousePos.y / RECT_HEIGHT)]
-    rect.onclick()
-    myContext.stroke()
- 
-}, false);
-
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -68,34 +47,34 @@ function getMousePos(canvas, evt) {
         y: evt.clientY - rect.top
     };
 }
-function getLiveNeighbours(x,y){
-     var liveNeighbours = 0;
-     if (grid[x+1][y+1].checked){
-         liveNeighbours ++;
-     }
-     if (grid[x-1][y+1].checked){
-         liveNeighbours ++;
-     }
-     if (grid[x-1][y-1].checked){
-         liveNeighbours ++;
-     }
-     if (grid[x+1][y-1].checked){
-         liveNeighbours ++;
-     }
+function getLiveNeighbours(x, y) {
+    var liveNeighbours = 0;
+    if (grid[x + 1][y + 1].checked) {
+        liveNeighbours++;
+    }
+    if (grid[x - 1][y + 1].checked) {
+        liveNeighbours++;
+    }
+    if (grid[x - 1][y - 1].checked) {
+        liveNeighbours++;
+    }
+    if (grid[x + 1][y - 1].checked) {
+        liveNeighbours++;
+    }
 
-     if (grid[x+1][y].checked){
-         liveNeighbours ++;
-     }
-     if (grid[x-1][y].checked){
-         liveNeighbours ++;
-     }
-     if (grid[x][y-1].checked){
-         liveNeighbours ++;
-     }
-     if (grid[x][y+1].checked){
-         liveNeighbours ++;
-     }
-     return liveNeighbours;
+    if (grid[x + 1][y].checked) {
+        liveNeighbours++;
+    }
+    if (grid[x - 1][y].checked) {
+        liveNeighbours++;
+    }
+    if (grid[x][y - 1].checked) {
+        liveNeighbours++;
+    }
+    if (grid[x][y + 1].checked) {
+        liveNeighbours++;
+    }
+    return liveNeighbours;
 }
 
 
@@ -104,59 +83,95 @@ function getLiveNeighbours(x,y){
 Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overpopulation.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.*/
-function gameOfLifeLoop(){
-        var cellsToDie = [];
-        var cellsToReproduce = [];
+function gameOfLifeLoop() {
+    var cellsToDie = [];
+    var cellsToReproduce = [];
 
     for (var i = 0; i < grid.length; i++) {
         for (var j = 0; j < grid[i].length; j++) {
             var cell = grid[i][j]
-            try{
-              var neigbourCount = getLiveNeighbours(i,j);
+            try {
+                var neigbourCount = getLiveNeighbours(i, j);
 
-            }catch(e){
-               // console.log(i + "," + j)
- 
+            } catch (e) {
+                // console.log(i + "," + j)
+
                 continue
 
             }
-           // console.log(neigbourCount)
-          // console.log(cell.checked)
-            if(cell.checked){
-                
+            // console.log(neigbourCount)
+            // console.log(cell.checked)
+            if (cell.checked) {
+
                 //live cell
-                if(neigbourCount < 2){
+                if (neigbourCount < 2) {
                     cellsToDie.push(cell)
                 }
-                if(neigbourCount>3){
+                if (neigbourCount > 3) {
                     cellsToDie.push(cell)
                 }
-            }else{
+            } else {
                 //dead cells
-                if(neigbourCount == 3){
+                if (neigbourCount == 3) {
                     cellsToReproduce.push(cell)
                 }
             }
         }
     }
-    for(var i = 0; i < cellsToDie.length; i++){
+    for (var i = 0; i < cellsToDie.length; i++) {
         console.log(i + "," + i + "dies")
         cellsToDie[i].checked = false;
         cellsToDie[i].draw()
 
 
     }
-     for(var i = 0; i < cellsToReproduce.length; i++){
+    for (var i = 0; i < cellsToReproduce.length; i++) {
         console.log(i + "," + i + "respawns")
 
         cellsToReproduce[i].checked = true;
         cellsToReproduce[i].draw()
     }
+    drawGrid();
     myContext.stroke();
- 
 }
+
+ 
+var myCanvas = document.getElementById("canvas");
+var myContext = myCanvas.getContext("2d");
+
+var RECT_WIDTH = 30
+var RECT_HEIGHT = RECT_WIDTH
+var GRID_SIZE = 50
+
+
+var BORDER_COLOUR = "#777673" //gray
+var ALIVE_COLOUR = "#000000" //white
+var DEAD_COLOUR = "#FFFFFF" //black
+
+
+var grid = Array(GRID_SIZE);
+for (var i = 0; i < grid.length; i++) {
+    grid[i] = Array(GRID_SIZE);
+}
+
+
+for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+        grid[i][j] = new Rectangle(myContext, i * RECT_WIDTH, j * RECT_HEIGHT)
+    }
+}
+myCanvas.addEventListener('mousedown', function (evt) {
+    console.log("mouse down")
+    var mousePos = getMousePos(canvas, evt);
+    var rect = grid[Math.floor(mousePos.x / RECT_WIDTH)][Math.floor(mousePos.y / RECT_HEIGHT)]
+    rect.onclick()
+    myContext.stroke()
+}, false);
+
+ 
+ /* to use */
 var buttonStart = document.getElementById("btnStart")
 var buttonPause = document.getElementById("btnPause")
-buttonStart.onclick = function(){intervalRef = setInterval(gameOfLifeLoop,1000)}
-
+buttonStart.onclick = function () { intervalRef = setInterval(gameOfLifeLoop, 1000) }
+drawGrid();
 myContext.stroke();
